d3434a382de1164a68aa10e0f390fa717603f20d
marvpaul <marvin.krueger@yahoo.de>
2018-01-17 13:43:59 +0100

    Added a super important output

--- Task7/src/main/java/Main.java
+++ Task7/src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
         Prim prim = new Prim();
+
+        System.out.println("Let's check some number to be prime numbers ^.^");
         for(int i = 0; i < 10000; i++){
             int randomNum = ThreadLocalRandom.current().nextInt(1000, Integer.MAX_VALUE- Integer.MAX_VALUE/2);
             System.out.println(randomNum + " prim? --> " + prim.isPrim(randomNum, 4));
