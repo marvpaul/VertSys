e96780b6aafc41c6afdebff4c92feeb8420ba058
marvpaul <marvin.krueger@yahoo.de>
2018-01-20 12:25:37 +0100

    Refactoing

--- Task7/src/main/java/Main.java
+++ Task7/src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Let's check some number to be prime numbers ^.^");
 
-        //Create 10.000 random numbers and check if the numbers are prims
+        //Create 10.000 random numbers and check if the numbers are primes
         for(int i = 0; i < 10000; i++){
             int randomNum = ThreadLocalRandom.current().nextInt(1000, Integer.MAX_VALUE- Integer.MAX_VALUE/2);
             System.out.println(randomNum + " prim? --> " + Prim.isPrim(randomNum, NR_THREADS));
--- Task7/src/main/java/Prim.java
+++ Task7/src/main/java/Prim.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class Prim {
+    private static ExecutorService executorService;
+    private static ArrayList<Future<Boolean>> tasks;
 
     /**
      * This method is for checking if a certain number is a prim number
@@ -10,43 +12,39 @@ public class Prim {
      * @return
      */
     public static boolean isPrim(int prim, int tasksToSplitIn){
-        //https://stackoverflow.com/questions/20221408/how-to-pass-a-parameter-to-a-thread-and-get-a-return-value
-        ExecutorService executorService = Executors.newFixedThreadPool(tasksToSplitIn);
+        //Create executor service
+        executorService = Executors.newFixedThreadPool(tasksToSplitIn);
 
+        int part_size = prim / tasksToSplitIn;
 
+        tasks = split_into_tasks_and_submit_job(part_size, prim);
 
-        int analyzeUpTo = prim;
-        int part_size = analyzeUpTo / tasksToSplitIn;
+        wait_for_termination();
 
+        return merge_results();
+    }
 
-
-        try {
-            executorService.awaitTermination(1, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        boolean isPrim = true;
-        ArrayList<Future<Boolean>> tasks = new ArrayList<Future<Boolean>>();
-        for(int i = 0; (i+1) * part_size <= analyzeUpTo; i++){
-            int start;
-            int end = part_size*(i+1);
-            if(i == 0){
-                start = part_size*i+2;
-            } else{
-                start = part_size*i+1;
-            }
-            //Start a new thread and add to thread array
-            tasks.add(executorService.submit(new ExecutablePrimFinder(start, end, prim)));
-
-        }
-        executorService.shutdown();
+    /**
+     * Wait until all tasks finished
+     */
+    private static void wait_for_termination(){
         //Try to shutdown and wait for all threads to be ready
+        executorService.shutdown();
         try {
             executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         } catch (InterruptedException e) {
             System.err.println("Task runs to long!");
         }
+    }
+
+    /**
+     * Merge all results
+     * @return return true if number is prim
+     */
+    private static boolean merge_results(){
+        boolean isPrim = true;
+
+        //iterate through each task, get result, combine the results
         for(Future<Boolean> task : tasks){
             try {
                 boolean partial_result = task.get(10, TimeUnit.MILLISECONDS);
@@ -57,7 +55,30 @@ public class Prim {
                 task.cancel(true);
             }
         }
-
         return isPrim;
+    }
+
+    /**
+     * Split the job into subtasks and execute these tasks
+     * @param part_size the amount of numbers each tasks has to check
+     * @param prim the number which could actually be a prim number
+     * @return the submitted tasks
+     */
+    private static ArrayList<Future<Boolean>> split_into_tasks_and_submit_job(int part_size, int prim){
+        ArrayList<Future<Boolean>> tasks = new ArrayList<>();
+
+        //Split into several tasks by defining some intervals
+        for(int i = 0; (i+1) * part_size <= prim; i++){
+            int start;
+            int end = part_size*(i+1);
+            if(i == 0){
+                start = part_size*i+2;
+            } else{
+                start = part_size*i+1;
+            }
+            //Start a new thread and add to thread array
+            tasks.add(executorService.submit(new ExecutablePrimFinder(start, end, prim)));
+        }
+        return tasks;
     }
 }
